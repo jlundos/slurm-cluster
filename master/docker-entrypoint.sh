1cbd58d54111dff1e57593d1485af16169e95682
Jens Lund <jlos@jlund.dk>
2024-07-10 19:22:05 +0200

    Be more careful with permissions on mungesetup directory to distribute munge.key

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -5,7 +5,7 @@ sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm/slurm.conf
 sudo service munge start
 
 # Store munge.key so it is available for the other nodes
-sudo mkdir -p ~admin/shared/mungesetup && sudo chmod a+rwx ~admin/shared/mungesetup
+sudo mkdir -p ~admin/shared/mungesetup && sudo chmod u+rwx,go+rx ~admin/shared/mungesetup
 sudo cp -a /etc/munge/munge.key ~admin/shared/mungesetup
 
 sudo service ssh start
