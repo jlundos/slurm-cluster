8f6a448afb292914a4d93634f9a9c709cf46fab2
Jens Lund <jlos@jlund.dk>
2024-09-01 14:17:50 +0200

    Clarify comment in job_1.sh

--- shared/job_1.sh
+++ shared/job_1.sh
@@ -5,7 +5,8 @@
 #
 #SBATCH --ntasks=24
 
-# Not necessary as we have first_test.py on a shared directory:
+# sbcast can be used to distribute files to nodes
+# Not necessary as we have test_1.py on a shared directory:
 ##sbcast -f test.py /tmp/test.py
 
 srun python3 test_1.py
